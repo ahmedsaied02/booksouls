64bc4ebd5e5eb506f60c135ac41f978daafaa0c8
abdel-17 <abdelrahmanb015@gmail.com>
2024-01-07 23:49:21 +0200

    style: reformat

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
+
 export default defineConfig({
 	plugins: [remix(), tsconfigPaths(), svgr()],
 });
