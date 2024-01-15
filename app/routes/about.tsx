784b2bb751ffbf6d86fa54ca5b29ebd0cc0bb590
ahmed saied <ahmed.saiedd02@gmail.com>
2024-01-15 22:24:28 +0200

    about us done

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -7,16 +7,16 @@ export default function About() {
 	return (
 		<>
 			<div className="bg-primary-text ">
-				<div className="absolute left-[80px] top-[50px] z-10 flex h-[620px] w-[1050px] flex-col items-center bg-secondary">
+				<div className="absolute left-[80px] top-[50px] z-10 flex h-[620px] w-[1130px] flex-col items-center bg-secondary">
 					<h1 className="py-5 text-4xl text-primary-text ">About Book Souls</h1>
 				</div>
 				<div className="absolute right-[80px] top-0 h-full w-[500px] bg-primary">
 					<Link to="/">
 						<HeaderLogo className="absolute right-6 top-0 z-20  w-40"></HeaderLogo>
 					</Link>
-					<div>
-						<h1>Meet The Creators</h1>
-						<div className="absolute z-30 grid h-full grid-cols-2 justify-center">
+					<div className="absolute left-64 top-20 z-30 p-7">
+						<h1 className="mb-5 text-primary-text text-xl">Meet The Creators</h1>
+						<div className="  grid h-full grid-cols-2 justify-center right-0 gap-7">
 							<Creator to="#" source={placeholder}></Creator>
 							<Creator to="#" source={placeholder}></Creator>
 							<Creator to="#" source={placeholder}></Creator>
@@ -33,8 +33,8 @@ export default function About() {
 }
 function Creator({ to, source }: { to: string; source: string }) {
 	return (
-		<li>
-			<Link to={to} className="rounded-full bg-primary-text ">
+		
+			<Link to={to}>
 				<img
 					src={source}
 					alt="creator"
@@ -43,6 +43,6 @@ function Creator({ to, source }: { to: string; source: string }) {
 					className="h-[80px] w-[80px] rounded-full"
 				/>
 			</Link>
-		</li>
+		
 	);
 }
