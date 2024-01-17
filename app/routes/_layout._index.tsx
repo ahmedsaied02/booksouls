4e96a689c715649264d8283823ab2b7845eaf74d
abdel-17 <abdelrahmanb015@gmail.com>
2024-01-17 16:02:20 +0200

    refactor: extract line gradient to component

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import placeholder from "~/assets/placeholder.jpeg";
 import Read from "~/assets/read.svg?react";
 import { IconButton } from "~/components/IconButton";
+import { LineGradient } from "~/components/LineGradient";
 
 function createFakeBook() {
 	return {
@@ -30,7 +31,7 @@ export default function Page() {
 			<section className="flex flex-col items-center bg-primary text-primary-text">
 				<h1 className="mt-12 text-center text-6xl font-thin uppercase">Get Engulfed</h1>
 				<Read className="mt-20" />
-				<div className="line-gradient" />
+				<LineGradient />
 			</section>
 			<section className="flex flex-col items-center bg-primary-text p-8 text-primary">
 				<h1 className="text-center text-7xl font-medium uppercase">Featured Books</h1>
